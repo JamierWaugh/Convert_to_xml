e84e0526d1422e905dfaa21d540ba23b5fd9d69a
Jamie Waugh <c4029539@newcastle.ac.uk.com>
2024-11-21 13:56:06 +0000

    Created saveToXml procedure Changed code into a procedure for more scalability, now any spreadsheet name and output file name could be provided and so long as they exist, the code will run

--- questionFinder.py
+++ questionFinder.py
@@ -1,18 +1,24 @@
 import pandas as pd
 from pathlib import Path
 
-path_load = Path(__file__).parent / "A level Questions.xlsx" #Generates relative path
-path_write = Path(__file__).parent / "output.xml"
+spreadsheet = "A level Questions.xlsx" #Added variables and function for more scalability
+output_file = "output.xml"
 
-df = pd.read_excel(path_load) #turn excel spreadsheet into panda dataframe
+def saveToXml(spreadsheet, output_file):
+    path_load = Path(__file__).parent / spreadsheet #Generates relative path
+    path_write = Path(__file__).parent / output_file
 
-xml_data = df.to_xml(root_name="data", row_name="record") #convert panda dataframe into xml
+    df = pd.read_excel(path_load) #turn excel spreadsheet into panda dataframe
 
-try:  #Error checking
-    # Save the XML string to a file
-    with path_write.open("w", encoding = "utf-8") as file: 
-        file.write(xml_data) #Writes data to output.xml
-        file.close()
-    print("DataFrame successfully saved to output.xml")
-except:
-    print("DataFrame failed to save to output.xml")
+    xml_data = df.to_xml(root_name="data", row_name="record") #convert panda dataframe into xml
+
+    try:  #Error checking
+        # Save the XML string to a file
+        with path_write.open("w", encoding = "utf-8") as file: 
+            file.write(xml_data) #Writes data to output.xml
+            file.close()
+        print("DataFrame successfully saved to output.xml")
+    except:
+        print("DataFrame failed to save to output.xml")
+
+saveToXml(spreadsheet, output_file)
